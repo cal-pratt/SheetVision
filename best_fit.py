3911dfee9293585aed75888dd0eecf7ca62bcac6
Cal Pratt <cpratt@mun.ca>
2016-08-11 18:08:59 -0230

    Find the best template match for a range or scales

---
+++ best_fit.py
@@ -0,0 +1,40 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def fit(img, templates, start_percent, stop_percent, threshold):
+    img_width, img_height = img.shape[::-1]
+    best_location_count = -1
+    best_locations = []
+    best_scale = 1
+
+    plt.axis([0, 2, 0, 1])
+    plt.show(block=False)
+
+    x = []
+    y = []
+    for scale in [i/100.0 for i in range(start_percent, stop_percent + 1, 3)]:
+        locations = []
+        location_count = 0
+        for template in templates:
+            template = cv2.resize(template, None,
+                fx = scale, fy = scale, interpolation = cv2.INTER_CUBIC)
+            result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+            result = np.where(result >= threshold)
+            location_count += len(result[0])
+            locations += [result]
+        print("scale: {0}, hits: {1}".format(scale, location_count))
+        x.append(location_count)
+        y.append(scale)
+        plt.plot(y, x)
+        plt.pause(0.00001)
+        if (location_count > best_location_count):
+            best_location_count = location_count
+            best_locations = locations
+            best_scale = scale
+            plt.axis([0, 2, 0, best_location_count])
+        elif (location_count < best_location_count):
+            pass
+    plt.close()
+
+    return best_locations, best_scale
