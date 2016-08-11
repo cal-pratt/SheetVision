cc25182fe4b3c695d6927ed103dbaa340fa4e332
Cal Pratt <cpratt@mun.ca>
2016-08-11 18:09:18 -0230

    Represent a rectangle

---
+++ rectangle.py
@@ -0,0 +1,34 @@
+import cv2
+import math
+
+class Rectangle(object):
+    def __init__(self, x, y, w, h):
+        self.x = x;
+        self.y = y;
+        self.w = w;
+        self.h = h;
+        self.middle = self.x + self.w/2, self.y + self.h/2
+        self.area = self.w * self.h
+
+    def overlap(self, other):
+        overlap_x = max(0, min(self.x + self.w, other.x + other.w) - max(self.x, other.x));
+        overlap_y = max(0, min(self.y + self.h, other.y + other.h) - max(self.y, other.y));
+        overlap_area = overlap_x * overlap_y
+        return overlap_area / self.area
+
+    def distance(self, other):
+        dx = self.middle[0] - other.middle[0]
+        dy = self.middle[1] - other.middle[1]
+        return math.sqrt(dx*dx + dy*dy)
+
+    def merge(self, other):
+        x = min(self.x, other.x)
+        y = min(self.y, other.y)
+        w = max(self.x + self.w, other.x + other.w) - x
+        h = max(self.y + self.h, other.y + other.h) - y
+        return Rectangle(x, y, w, h)
+
+    def draw(self, img, color, thickness):
+        pos = ((int)(self.x), (int)(self.y))
+        size = ((int)(self.x + self.w), (int)(self.y + self.h))
+        cv2.rectangle(img, pos, size, color, thickness)
